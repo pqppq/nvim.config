4da13a4ac5f2eb3a8dcc6b3ad7ac2830f9637cdc
pqppq <yutatsu0119@gmail.com>
2025-10-28 21:15:54 +0900

    chore: remove unused lines

--- lua/lazyvim/plugins/helper.lua
+++ lua/lazyvim/plugins/helper.lua
@@ -30,9 +30,6 @@ return {
 				enhanced_diff_hl = true,
 				use_icons = true,
 				view = {
-					-- default = {
-					-- 	layout = "diff3_mixed"
-					-- },
 					merge_tool = {
 						layout = "diff3_mixed",
 						winbar_info = false,
@@ -81,7 +78,6 @@ return {
 		keys = {
 			{ "<Space>s", "<cmd>HopWord<CR>" },
 			{ "<Space>l", "<cmd>HopWordCurrentLine<CR>", mode = { "n", "v" } },
-			-- { "<Space>S", "<cmd>HopChar1<cr>" },
 			{ "<Space>k", "<cmd>HopLine<CR>" },
 		},
 		config = function()
@@ -102,25 +98,6 @@ return {
 			},
 		},
 		keys = {
-			-- {
-			-- 	"<Space>k",
-			-- 	mode = { "n" },
-			-- 	function()
-			-- 		require("flash").jump({
-			-- 			search = { mode = "search", max_length = 0 },
-			-- 			label = { after = { 0, 0 } },
-			-- 			pattern = "^"
-			-- 		})
-			-- 	end,
-			-- 	desc = "Flash",
-			-- },
-			-- {
-			-- 	"<Space>S",
-			-- 	mode = { "n" },
-			-- 	function()
-			-- 		require("flash").jump()
-			-- 	end,
-			-- },
 			{
 				"<Space>v",
 				mode = { "n" },
@@ -140,8 +117,6 @@ return {
 		},
 		keys = {
 			{ "fs", ":NvimTreeToggle<CR>" },
-			-- { "fd", ":Neotree toggle show buffers<CR>" },
-			-- { "fg", ":Neotree toggle show git_status<CR>" },
 		},
 		config = function()
 			-- disable netrw at the very start of your init.lua
@@ -171,9 +146,7 @@ return {
 				vim.keymap.set('n', 'D', api.fs.trash, opts('Trash'))
 				vim.keymap.set('n', 'e', api.tree.expand_all, opts('Expand All'))
 				vim.keymap.set('n', 'E', api.tree.collapse_all, opts('Collapse'))
-				vim.keymap.set('n', '<Tab>', api.node.open.preview, opts('Open Preview'))
-				-- vim.keymap.set('n', 'p', api.fs.paste, opts('Paste'))
-				-- vim.keymap.set('n', 'P', api.node.navigate.parent, opts('Parent Directory'))
+				vim.keymap.set('n', 'P', api.node.navigate.parent, opts('Parent Directory'))
 				vim.keymap.set('n', 'q', api.tree.close, opts('Close'))
 				vim.keymap.set('n', 'fs', api.tree.close, opts('Close'))
 				vim.keymap.set('n', 'r', api.fs.rename, opts('Rename'))
@@ -187,21 +160,15 @@ return {
 				vim.keymap.set('n', '<C-e>', api.node.open.replace_tree_buffer, opts('Open: In Place'))
 				vim.keymap.set('n', '<C-k>', api.node.show_info_popup, opts('Info'))
 				vim.keymap.set('n', '<C-r>', api.fs.rename_sub, opts('Rename: Omit Filename'))
-				-- vim.keymap.set('n', '<C-t>', api.node.open.tab, opts('Open: New Tab'))
 				vim.keymap.set('n', '<C-v>', api.node.open.vertical, opts('Open: Vertical Split'))
 				vim.keymap.set('n', '<BS>', api.node.navigate.parent_close, opts('Close Directory'))
 				vim.keymap.set('n', '<CR>', api.node.open.no_window_picker, opts('Open'))
 				vim.keymap.set('n', '<C-o>', api.node.open.no_window_picker, opts('Open'))
-				-- vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
-				-- vim.keymap.set('n', '>', api.node.navigate.sibling.next, opts('Next Sibling'))
-				-- vim.keymap.set('n', '<', api.node.navigate.sibling.prev, opts('Previous Sibling'))
-				-- vim.keymap.set('n', '.', api.node.run.cmd, opts('Run Command'))
 				vim.keymap.set('n', 'B', api.tree.toggle_no_buffer_filter, opts('Toggle No Buffer'))
 				vim.keymap.set('n', 'c', api.fs.copy.node, opts('Copy'))
 				vim.keymap.set('n', 'C', api.tree.toggle_git_clean_filter, opts('Toggle Git Clean'))
 				vim.keymap.set('n', '[c', api.node.navigate.git.prev, opts('Prev Git'))
 				vim.keymap.set('n', ']c', api.node.navigate.git.next, opts('Next Git'))
-				-- vim.keymap.set('n', 'e', api.fs.rename_basename, opts('Rename: Basename'))
 				vim.keymap.set('n', ']e', api.node.navigate.diagnostics.next, opts('Next Diagnostic'))
 				vim.keymap.set('n', '[e', api.node.navigate.diagnostics.prev, opts('Prev Diagnostic'))
 				vim.keymap.set('n', 'F', api.live_filter.clear, opts('Clean Filter'))
@@ -211,14 +178,11 @@ return {
 				vim.keymap.set('n', 'J', api.node.navigate.sibling.last, opts('Last Sibling'))
 				vim.keymap.set('n', 'K', api.node.navigate.sibling.first, opts('First Sibling'))
 				vim.keymap.set('n', 'R', api.tree.reload, opts('Refresh'))
-				-- vim.keymap.set('n', 's', api.node.run.system, opts('Run System'))
 				vim.keymap.set('n', 'S', api.tree.search_node, opts('Search'))
 				vim.keymap.set('n', 'U', api.tree.toggle_custom_filter, opts('Toggle Hidden'))
 				vim.keymap.set('n', 'x', api.fs.cut, opts('Cut'))
 				vim.keymap.set('n', 'y', api.fs.copy.filename, opts('Copy Name'))
 				vim.keymap.set('n', 'Y', api.fs.copy.relative_path, opts('Copy Relative Path'))
-				-- vim.keymap.set('n', '<2-LeftMouse>', api.node.open.edit, opts('Open'))
-				-- vim.keymap.set('n', '<2-RightMouse>', api.tree.change_root_to_node, opts('CD'))
 			end
 
 			-- OR setup with some options
