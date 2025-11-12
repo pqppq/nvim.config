0a2d1be84c9baad9431f6542699d232f50c89e1d
pqppq <57561531+pqppq@users.noreply.github.com>
2025-11-12 10:37:33 +0900

    chore: make hop.nvim binds noremap

--- lua/lazyvim/plugins/helper.lua
+++ lua/lazyvim/plugins/helper.lua
@@ -40,9 +40,9 @@ return {
 	{
 		'phaazon/hop.nvim',
 		keys = {
-			{ "<Space>s", "<cmd>HopWord<CR>" },
-			{ "<Space>l", "<cmd>HopWordCurrentLine<CR>", mode = { "n", "v" } },
-			{ "<Space>k", "<cmd>HopLine<CR>" },
+			{ "<Space>s", "<cmd>HopWord<CR>", mode = { "n", "v" }, noremap = true },
+			{ "<Space>l", "<cmd>HopWordCurrentLine<CR>", mode = { "n", "v" }, noremap = true },
+			{ "<Space>k", "<cmd>HopLine<CR>", mode = { "n", "v" }, noremap = true },
 		},
 		config = function()
 			require('hop').setup({})
