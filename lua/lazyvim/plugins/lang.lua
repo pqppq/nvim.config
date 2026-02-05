b07af4f757bb5c288c6c171f7d7789e69d2fdc35
pqppq <yutatsu.oe@roms.inc>
2026-02-05 11:55:42 +0900

    chore: remove CmdlineEnter event for go.nvim

--- lua/lazyvim/plugins/lang.lua
+++ lua/lazyvim/plugins/lang.lua
@@ -31,7 +31,6 @@ return {
 				group = format_sync_grp,
 			})
 		end,
-		event = { "CmdlineEnter" },
 		build = ":lua require('go.install').update_all_sync()" -- if you need to install/update all binaries
 	},
 	{
