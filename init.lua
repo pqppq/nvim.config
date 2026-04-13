9c33ddf69a97bc55520d37f0e49e09fd28ad4e71
pqppq <yutatsu.oe@roms.inc>
2026-04-13 09:27:44 +0900

    feat: set vim.opt.number true

--- init.lua
+++ init.lua
@@ -26,6 +26,7 @@ vim.cmd("cnoreabbrev <expr> s getcmdtype() .. getcmdline() ==# ':s' ? [getchar()
 
 vim.g.netrw_http_cmd = 'open'
 vim.opt.laststatus = 2
+vim.opt.number = true
 vim.opt.statusline = '%t'
 vim.opt.statusline:append '%m'
 vim.opt.statusline:append '%='
